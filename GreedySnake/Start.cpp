416a86aeaa1a78c1cb6839a116cc8fa822cab4b4
Zou Tao <928302279@qq.com>
2017-05-31 21:45:09 +0800

    添加音效以及重新开始和退出功能

--- GreedySnake/Start.cpp
+++ GreedySnake/Start.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include"acllib.h"
 #include <time.h>
 #include "food.h"
@@ -7,20 +8,34 @@ ACL_Image mapImage;
 ACL_Image snakeImage;
 ACL_Image foodImage;
 ACL_Image background;
+ACL_Sound Eat;
+ACL_Sound Failed;
 int Scored;
-BOOL isAgain;
+BOOL isOver;
 BOOL flag;
 Map map;
 food foood;
 Snake snake;
 void keyEvent(int key, int event);
 void timerEvent(int id);
+void init();
 int Setup() {
 	srand((unsigned)time(NULL));
-	initWindow("GreedySnake", DEFAULT, DEFAULT, 750, 600);
-	Scored = 0;
+	initWindow("GreedySnake", 100, 50, 750, 600);
+	loadSound("Failed.wav", &Failed);
+	loadSound("Eat.wav",&Eat);
 	map = Map();
-	map.initMap(mapImage,background);
+	foood = food();
+	snake = Snake();
+	registerKeyboardEvent(keyEvent);
+	registerTimerEvent(timerEvent);
+	init();
+	return 0;
+}
+
+void init() {
+	Scored = 0;
+	map.initMap(mapImage, background);
 	char num[20];
 	sprintf(num, "%d", 0);
 	beginPaint();
@@ -29,15 +44,11 @@ int Setup() {
 	setTextBkColor(ACL_Color BLACK);
 	paintText(690, 40, num);
 	endPaint();
-	foood = food();
-	foood.x = (rand() % 28 + 1)*20;
-	foood.y = (rand() % 28 + 1)*20;
+	foood.x = (rand() % 28 + 1) * 20;
+	foood.y = (rand() % 28 + 1) * 20;
 	foood.drawFood(foodImage);
-	snake = Snake();
 	snake.initSnake(snakeImage);
-	registerKeyboardEvent(keyEvent);
-	registerTimerEvent(timerEvent);
-	return 0;
+	isOver = FALSE;
 }
 
 void timerEvent(int id) {
@@ -47,23 +58,10 @@ void timerEvent(int id) {
 		snake.list.pop_back();					
 	}
 	else {
-		PointList::iterator i;
 		Scored += 1;
-		isAgain = TRUE;
-		while (isAgain)
-		{
-			foood.x = (rand() % 28 + 1) * 20;
-			foood.y = (rand() % 28 + 1) * 20;
-			isAgain = FALSE;
-			for (i = snake.list.begin();i != snake.list.end();++i)
-			{
-				if ((*i).x == foood.x && (*i).y == foood.y)
-				{
-					isAgain = TRUE;
-					break;
-				}
-			}
-		}
+		playSound(Eat, 0);
+		foood.x = (rand() % 28 + 1) * 20;
+		foood.y = (rand() % 28 + 1) * 20;
 	}
 	beginPaint();
 	char num[20];
@@ -79,17 +77,23 @@ void timerEvent(int id) {
 	snake.move(foood);
 	snake.DrawSnake(snakeImage);
 	if (snake.isOverEdge() || snake.isEatItself()) {
+		playSound(Failed, 0);
 		cancelTimer(0);
 		beginPaint();
 		char num[20];
+		char reStart[27] = "Enter 学什么学我还要玩！";
+		char exit[24] = "Esc   不玩了 我要去学习";
 		sprintf(num, "%d", Scored);
 		char result[20] = "游戏结束 得分：";
 		setTextColor(ACL_Color WHITE);
 		setTextSize(20);
 		setTextBkColor(ACL_Color BLACK);
 		paintText(400, 280, num);
+		paintText(210, 340, reStart);
+		paintText(210, 310, exit);
 		paintText(240,280,result);
 		endPaint();
+		isOver = TRUE;
 	}
 }
 
@@ -102,19 +106,36 @@ void keyEvent (int key, int event) {
 		}
 		if (key== VK_ESCAPE)
 		{
-			if (flag)
+			if (isOver==TRUE)
 			{
-				cancelTimer(0);
+				exit(0);
 			}
 			else
 			{
-				startTimer(0, 500);
+				if (flag)
+				{
+					cancelTimer(0);
+					flag = FALSE;
+				}
+				else
+				{
+					startTimer(0, 100);
+					flag = TRUE;
+				}
 			}
 		}
 		if (key==VK_RETURN)
 		{
-			startTimer(0,100);
-			flag = TRUE;
+			if (isOver == FALSE)
+			{
+				startTimer(0, 100);
+				flag = TRUE;
+			}
+			else
+			{
+				snake.list.clear();
+				init();
+			}
 		}
 	}
 }
